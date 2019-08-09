393f331ae654a11b4937e6c9d323b8d928b0d0b9
George Armstrong <garmstrong@colgate.edu>
2019-08-09 14:12:47 -0700

    initial project structuring

---
+++ benchutils/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['abserror', 'abserrorplot', 'profilecorr', 'profilecorrplot', 'utils']
---
+++ benchutils/abserror.py
@@ -0,0 +1,4 @@
+import os
+
+def profile(observed_file, expected_file, output_file):
+    os.system('touch {}'.format(output_file))
