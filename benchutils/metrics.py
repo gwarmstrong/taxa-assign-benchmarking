3b07021cee5cf77ea1c3da18e1eeb26db7f3647d
gwarmstrong <garmstrong@colgate.edu>
2019-08-12 12:47:58 -0700

    restrucutre plotting and metrics

---
+++ benchutils/metrics.py
@@ -0,0 +1,9 @@
+import os
+
+
+def absolute_error(observed_file, expected_file, output_file):
+    os.system('touch {}'.format(output_file))
+
+
+def correlation(observed_file, expected_file, output_file):
+    os.system('touch {}'.format(output_file))
---
+++ benchutils/plotting.py
@@ -0,0 +1,9 @@
+import os
+
+
+def absolute_error_plot(absolute_error_file, output_file):
+    os.system('touch {}'.format(output_file))
+
+
+def correlation_plot(correlation_file, output_file):
+    os.system('touch {}'.format(output_file))
