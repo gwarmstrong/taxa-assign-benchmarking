5ee5d1ab217b73e1832d5c5abb9525028b27b0eb
George Armstrong <garmstrong@colgate.edu>
2019-08-15 15:21:53 -0700

    todo's, error handling, and imports

--- benchutils/metrics.py
+++ benchutils/metrics.py
@@ -1,9 +1,16 @@
 import os
+import pandas as pd
+from benchutils import ranks
 
 
 def absolute_error(observed_file, expected_file, output_file, rank):
+    # TODO use rank to subset the expected true profile
+    if rank not in ranks:
+        raise ValueError('Rank \'{}\' not in available ranks'.format(rank))
     os.system('touch {}'.format(output_file))
 
 
 def correlation(observed_file, expected_file, output_file, rank):
+    if rank not in ranks:
+        raise ValueError('Rank \'{}\' not in available ranks'.format(rank))
     os.system('touch {}'.format(output_file))
