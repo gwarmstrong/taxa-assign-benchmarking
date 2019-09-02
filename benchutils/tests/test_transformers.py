80589d026397ef218a5c607fb9729b4e21e01dce
George Armstrong <garmstrong@colgate.edu>
2019-09-02 14:43:35 -0700

    metaphlan2 transformer and transformer testing outline

---
+++ benchutils/tests/test_transformers.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestMetaphlan2Transformer(unittest.TestCase):
+    # TODO figure out how to get absolute paths to test data directory and
+    #  delete files after done running unit tests
+    pass
+
+
+class TestKraken2Transformer(unittest.TestCase):
+    # TODO needs sample input
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
--- benchutils/transformers.py
+++ benchutils/transformers.py
@@ -2,14 +2,24 @@ import os
 import pandas as pd
 
 kraken_rank_dictionary = {
-    'C': 'class',
     'P': 'phylum',
+    'C': 'class',
+    'O': 'order',
     'F': 'family',
     'G': 'genus',
-    'O': 'order',
     'S': 'species'  # TODO convert to strain to species using ETE toolkit (it
     # gets kind of tricky because kraken gives pretty high res species
     # estimates
+}
+
+greengenes_rank_dict = {
+    'k__': 'kingdom',
+    'p__': 'phylum',
+    'c__': 'class',
+    'o__': 'order',
+    'f__': 'family',
+    'g__': 'genus',
+    's__': 'species'
 }
 
 kraken_columns = ['PERCENTAGE', 'lca_read_count', 'read_count', 'rank',
@@ -42,6 +52,22 @@ def kraken2_transformer(all_rank_summary, output_rank_summaries, ranks):
     all_ranks = all_ranks.loc[all_ranks['rank'].isin(kraken_rank_dictionary)]
     all_ranks['RANK'] = [kraken_rank_dictionary[key] for key in
                          all_ranks['rank']]
+    keep_cols = ['@@TAXID', 'RANK', 'TAXNAME', 'PERCENTAGE']
+    for output_, rank in zip(output_rank_summaries, ranks):
+        sub_df = all_ranks.loc[all_ranks['RANK'] == rank]
+        sub_df_matching = sub_df[keep_cols]
+        sub_df_matching.to_csv(output_, sep='\t', index=False)
+
+
+def metaphlan2_transfomer(all_rank_summary, output_rank_summaries, ranks):
+    all_ranks = pd.read_csv(all_rank_summary, sep='\t', skiprows=3)
+    def last_entry(x): return x.split('|')[-1]
+    all_ranks['last_clade'] = all_ranks['#clade_name'].map(last_entry)
+    all_ranks['@@TAXID'] = all_ranks['NCBI_tax_id'].map(last_entry)
+    all_ranks['RANK'] = all_ranks['last_clade'].map(
+        lambda x: greengenes_rank_dict[x[:3]])
+    all_ranks['TAXNAME'] = all_ranks['low_clade'].map(lambda x: x[3:])
+
     keep_cols = ['@@TAXID', 'RANK', 'TAXNAME', 'PERCENTAGE']
     for output_, rank in zip(output_rank_summaries, ranks):
         sub_df = all_ranks.loc[all_ranks['RANK'] == rank]
