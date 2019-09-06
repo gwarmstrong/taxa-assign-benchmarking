09bb34878930337c45d3be0cb245d196601469d6
George Armstrong <garmstrong@colgate.edu>
2019-09-06 14:58:58 -0700

    test for different date in ci

--- ci/setup_demo_data.sh
+++ ci/setup_demo_data.sh
@@ -1,7 +1,10 @@
-mkdir -p data/simulations/test-sim{1,2,3}/fake_date_sample_{0,1}/reads/
+mkdir -p data/simulations/test-sim{1,2}/fake_date_sample_{0,1}/reads/
+mkdir -p data/simulations/test-sim3/fake_date3_sample_{0,1}/reads/
 echo '' > data/simulations/test-sim1/fake_date_sample_0/reads/anonymous_reads.fq.gz
 echo '' > data/simulations/test-sim2/fake_date_sample_0/reads/anonymous_reads.fq.gz
-echo '' > data/simulations/test-sim/fake_date_sample_1/reads/anonymous_reads.fq
+echo '' > data/simulations/test-sim1/fake_date_sample_1/reads/anonymous_reads.fq
+echo '' > data/simulations/test-sim3/fake_date3_sample_0/reads/anonymous_reads.fq
 echo '' > data/simulations/test-sim1/taxonomic_profile_0.txt
 echo '' > data/simulations/test-sim2/taxonomic_profile_0.txt
-echo '' > data/simulations/test-sim/taxonomic_profile_1.txt
+echo '' > data/simulations/test-sim1/taxonomic_profile_1.txt
+echo '' > data/simulations/test-sim3/taxonomic_profile_0.txt
